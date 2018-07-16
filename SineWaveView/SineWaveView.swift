db21890900d6637df13c998bb4c0fec9fbdd7505
Hugo Laloge <hugo.laloge@gmail.com>
2018-07-16 11:31:15 +0200

    feat: try bezier curves

--- SineWaveView/SineWaveView.swift
+++ SineWaveView/SineWaveView.swift
@@ -10,31 +10,36 @@ public class SineWaveView: UIView {
 
     private func updateWaves() {
         layer.sublayers?.forEach { $0.removeFromSuperlayer() }
-        let fromPath = buildPath(withAmplitude: 1.0, frequency: 0.5, inRect: bounds)
 
         for wave in waves {
-            let path = buildPath(withAmplitude: wave.amplitude,
-                                 frequency: wave.frequency,
-                                 inRect: bounds)
-
-            let pathLayer = CAShapeLayer()
-            pathLayer.frame = bounds
-            pathLayer.path = path.cgPath
-            pathLayer.strokeColor = wave.color.cgColor
-            pathLayer.fillColor = nil
-            pathLayer.lineWidth = 2
-            pathLayer.lineJoin = kCALineJoinBevel
-            pathLayer.strokeStart = 0.0
-            layer.addSublayer(pathLayer)
-
-            let animation = CABasicAnimation(keyPath: "path")
-            animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-            animation.fromValue = fromPath.cgPath
-            animation.toValue = path.cgPath
-            animation.duration = 4.0
-
-            pathLayer.add(animation, forKey: "path")
+            addShapeLayer(forPath: buildPath(withAmplitude: wave.amplitude,
+                                             frequency: wave.frequency,
+                                             inRect: bounds).cgPath,
+                          color: wave.color.cgColor)
+            addShapeLayer(forPath: buildBezierPath(withAmplitude: wave.amplitude,
+                                                   frequency: wave.frequency,
+                                                   inRect: bounds).cgPath,
+                          color: wave.color.withAlphaComponent(0.5).cgColor)
         }
+    }
+
+    private func addShapeLayer(forPath path: CGPath, color: CGColor) {
+        let basicPath = buildBezierPath(withAmplitude: 1.0, frequency: 0.5, inRect: bounds)
+        let pathLayer = CAShapeLayer()
+        pathLayer.frame = bounds
+        pathLayer.path = path
+        pathLayer.strokeColor = color
+        pathLayer.fillColor = nil
+        pathLayer.lineWidth = 2
+        pathLayer.lineJoin = kCALineJoinBevel
+        pathLayer.strokeStart = 0.0
+        layer.addSublayer(pathLayer)
+
+        let animation = CABasicAnimation(keyPath: "path")
+        animation.fromValue = basicPath.cgPath
+        animation.toValue = path
+        animation.duration = 3
+        pathLayer.add(animation, forKey: nil)
     }
 
     private func buildPath(withAmplitude amplitude: CGFloat, frequency: CGFloat, inRect rect: CGRect) -> UIBezierPath {
@@ -49,10 +54,32 @@ public class SineWaveView: UIView {
         for point in 0...precision {
             let advancement = CGFloat(point) / CGFloat(precision)
             let x = origin.x + advancement * width
-            let y = origin.y - CGFloat(sin(advancement * .pi * frequency)) * height * amplitude / 2
+            let y = origin.y - CGFloat(sin(advancement * .pi * frequency * 2.0)) * height * amplitude / 2
             path.addLine(to: CGPoint(x: x, y: y))
         }
 
+        return path
+    }
+
+    private func buildBezierPath(withAmplitude amplitude: CGFloat, frequency: CGFloat, inRect rect: CGRect) -> UIBezierPath {
+        let width = rect.width
+        let height = rect.height
+        let origin = CGPoint(x: -0.5 / (frequency * 2.0) * width, y: height / 2.0)
+        let nbpoints = Int(ceil(frequency * 2)) + 1
+
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: -1.5 / (frequency * 2.0) * width, y: -amplitude / 2.0 * height))
+
+        for i in 0...nbpoints {
+            let deltaY = amplitude / 2.0 * height
+            let previousY = origin.y + (i % 2 == 1 ? deltaY : -deltaY)
+            let previousX = origin.x + CGFloat(i - 1) / (frequency * 2.0) * width
+            let x = origin.x + CGFloat(i) / (frequency * 2.0) * width
+            let y = origin.y + (i % 2 == 0 ? deltaY : -deltaY)
+            path.addCurve(to: CGPoint(x: x, y: y),
+                          controlPoint1: CGPoint(x: (previousX + x) / 2.0, y: previousY),
+                          controlPoint2: CGPoint(x: (previousX + x) / 2.0, y: y))
+        }
         return path
     }
 }
